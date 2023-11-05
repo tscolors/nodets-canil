01750f1509e2f315bd38f670afe8e760b64cc9fb
TIAGO <tscolors@hotmail.com>
2023-11-05 15:07:58 -0300

    1º primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # nodets-canil
+
+Projeto feito por Tiago MasterDev
+
+
+### Pré-requisito globais
+"npm i -g nodemon typescript ts-node"
+
+
+### Instalação
+"npm install"
+
+
+### Para rodar o projeto
+"npm run start-dev"
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mustache from 'mustache-express'
+import path from 'path'
+import mainRoutes from './routes/index'
+
+dotenv.config()
+
+const server = express()
+server.set('view engine',  'mustache')
+server.set('views', path.join(__dirname, 'views'))
+const port = process.env.PORT
+server.engine('mustache', mustache())
+server.use(express.static(path.join(__dirname, '../public')))
+
+server.use(mainRoutes)
+server.use((req, res)=>
+{
+    res.send('Page not found!')
+})
+
+
+server.listen(port, ()=>{console.log("Server running on port: 3000")})
